751333a32507eb9e3412b15c46b2952d2c2e183e
Zaryab Haider <haiderzaryab71@gmail.com>
2021-01-05 04:19:32 +0500

    Setup Database  , Design Navbar and Register route page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,45 @@
     <title>Posty</title>
     <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
-<body>
+<body class="bg-gray-200">
+    
+        <nav class="p-6 bg-white flex justify-between mb-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-3 font-medium">Home</a>
+                </li>
+                
+                <li>
+                    <a href="" class="p-3 font-medium">Dashboard</a>
+                </li>
+
+                <li>
+                    <a href="" class="p-3 font-medium">Post</a>
+                </li>
+            </ul>
+
+
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-3 bold font-bold">Zaryab Haider</a>
+                </li>
+                
+                <li>
+                    <a href="" class="p-3 font-medium">Login</a>
+                </li>
+
+                <li>
+                    <a href="{{ route('register')}}" class="p-3 font-medium">Register</a>
+                </li>
+
+                <li>
+                    <a href="" class="p-3 font-medium">logout</a>
+                </li>
+            </ul>
+        </nav>
+
+
+
 
     @yield('content')
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    index
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg font-medium">
+            Posts
+        </div>
+    </div>
 
 @endsection
